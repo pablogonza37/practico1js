e9f0f8c2db911e0c3f87e4a57d0534dd2a39ed95
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-25 00:21:07 -0300

    ejercicio 3 resuelto

---
+++ js/ejercicio3.js
@@ -0,0 +1,10 @@
+// variable  en grados Celsius
+let gradosCelsius = 30;
+
+// Calcular grados Fahrenheit 
+let gradosFahrenheit = (gradosCelsius * 9/5) + 32;
+
+// Mostrar los valores 
+
+console.log("Grados Celsius:", gradosCelsius);
+console.log("Grados Fahrenheit:", gradosFahrenheit);
