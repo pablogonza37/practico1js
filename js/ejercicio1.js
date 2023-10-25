67d372277e6a07775b81f50a8d10f6508b623bdc
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-24 21:44:42 -0300

    ejercicio 1 realizado

---
+++ js/ejercicio1.js
@@ -0,0 +1,15 @@
+// Crear dos variables y asignarles valores numéricos
+let numero1 = 25;
+let numero2 = 5;
+
+// Realizar las operaciones
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+// Mostrar los resultados en la consola
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multiplicación:", multiplicacion);
+console.log("División:", division);
