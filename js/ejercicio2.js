d95c9164e7c4c30ede083c333837ee8885bfcef3
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-25 00:16:28 -0300

    ejercicio 2 resuelto

---
+++ js/ejercicio2.js
@@ -0,0 +1,12 @@
+// Variables
+
+let nombre = "Pablo";
+let apellido = "Gonzalez";
+
+// Concatenar variables
+
+let nombreCompleto = "Mi nombre completo es " + nombre + " " + apellido;
+
+// Mostrar variable 
+
+console.log(nombreCompleto);
