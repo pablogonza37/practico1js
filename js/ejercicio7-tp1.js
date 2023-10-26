3d6f2c64587b5b1fe7142e48cb01937c07047dab
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-26 19:37:55 -0300

    ejercicio 7 del tp 1 resuelto

---
+++ js/ejercicio7-tp1.js
@@ -0,0 +1,29 @@
+/* 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+Ejemplo: 
+
+input: 15 , 3, 9
+
+
+Output: El 15 es el número más grande
+*/
+
+let numero1 = parseInt(prompt("Ingrese el primer número: "));
+let numero2 = parseInt(prompt("Ingrese el segundo número: "));
+let numero3 = parseInt(prompt("Ingrese el tercer número: "));
+
+
+let numero_mayor = numero1
+
+
+if (numero2 > numero_mayor){
+    numero_mayor = numero2;
+}   
+
+if (numero3 > numero_mayor){
+    numero_mayor = numero3;
+}
+
+console.log(`El mayor de los 3 numeros es el ${numero_mayor}`);
+
+
+
