7374f2e20440ced5cfd06e6435b9c2a920393648
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-26 19:05:00 -0300

    ejercicio 6 del tp 1 resuelto

---
+++ js/ejercicio6-tp1.js
@@ -0,0 +1,17 @@
+/* Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+Ejemplo: 
+
+input: 15 , 3
+
+
+Output: El 15 es el número más grande
+*/
+
+let numeroA = parseInt(prompt("digite un numero: "));
+let numeroB = parseInt(prompt("digite otro numero: "));
+
+if (numeroA > numeroB){
+    console.log(`El mayor de los numeros es el ${numeroA}`);
+}else {
+    console.log(`El mayor de los numeros es el ${numeroB}`);
+}
