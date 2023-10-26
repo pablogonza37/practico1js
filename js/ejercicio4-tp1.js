97519b18cee93f8352e7eb97f5fd75a4a40181d6
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-26 15:23:40 -0300

    ejercico 4 del tp1 resuelto

---
+++ js/ejercicio4-tp1.js
@@ -0,0 +1,12 @@
+/*4.- Escribe un programa de dos líneas que pida el nombre
+ del usuario con un prompt y escriba un texto que diga
+  «Hola nombreUsuario»
+Ejemplo: 
+
+input: Coder 
+Output: Hola Coder
+*/
+
+nombre = prompt("escriba su nombre: ");
+
+console.log("hola ", nombre);
