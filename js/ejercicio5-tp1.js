345c9c40d23b322efcd647b0af00579ab5b9f746
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-26 18:22:49 -0300

    ejercico 5 del tp 1 resuelto

---
+++ js/ejercicio5-tp1.js
@@ -0,0 +1,11 @@
+/* 5.- Escribe un programa de tres líneas que pida un número,
+ pida otro número y escriba el resultado de sumar estos dos números.
+
+Nota: Tener en cuenta la siguiente función: parseInt
+*/
+
+let numeroA = parseInt(prompt("digite un numero: "));
+
+let numeroB = parseInt(prompt("digite otro numero: "));
+
+console.log("la suma es: ", numeroA + numeroB);
