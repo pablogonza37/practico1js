7f675d84cbfcd17ee4e927510fdc29065c4f124d
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-28 00:46:28 -0300

    ejercicio 10 corregido

--- js/ejercicio10-tp1.js
+++ js/ejercicio10-tp1.js
@@ -6,23 +6,15 @@ input: 20
 Output: El 20 es divisible por 2.
 */
 
-
 let numero = parseInt(prompt("Ingrese un numero: "));
 
-
-if (numero % 2 === 0){
-    alert(`${numero} es divisible por 2`);
+if (
+  numero % 2 === 0 ||
+  numero % 3 === 0 ||
+  numero % 5 === 0 ||
+  numero % 7 === 0
+) {
+  alert(`${numero} es divisible por alguno de estos 4 numeros`);
+} else {
+  alert(`${numero} no es divisible por 2, 3, 5 ni 7`);
 }
-else if (numero % 3 === 0){
-    alert(`${numero} es divisible por 3`);
-}
-else if (numero % 5 === 0){
-    alert(`${numero} es divisible por 5`);
-}
-else if (numero % 7 === 0){
-    alert(`${numero} es divisible por 7`);
-}
-else{
-    alert(`${numero} no es divisible por 2, 3, 5 ni 7`);
-}
-
