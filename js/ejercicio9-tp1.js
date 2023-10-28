527c20c942007e3d52d9d89dd1d2cbf785e0ad9d
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2023-10-27 21:12:26 -0300

    ejercicio 9 del tp 1 resuleto

---
+++ js/ejercicio9-tp1.js
@@ -0,0 +1,23 @@
+/* 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+Ejemplo:
+
+
+
+input: Hola mundo
+Output: oauo
+*/
+
+let cadena = prompt("escriba una frase: ");
+
+for (i = 0; i < cadena.length; i++) {
+  if (
+    cadena.charAt(i) == "a" ||
+    cadena.charAt(i) == "e" ||
+    cadena.charAt(i) == "i" ||
+    cadena.charAt(i) == "o" ||
+    cadena.charAt(i) == "u"
+  ) {
+    console.log(cadena.charAt(i));
+  }
+}
